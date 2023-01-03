c03d2ca6ec8a630547e512075510c67fac7c4c76
Haru <frankcito639@gmail.com>
2023-01-03 18:10:27 -0500

    Ruta Restaurantes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,14 +1,17 @@
 const { Router } = require("express");
+const restaurants = require("./restaurants");
 
 // import all controllers
 // import SessionController from './app/controllers/SessionController';
 
-const routes = new Router();
+const routes = Router();
 
 // Add routes
 // routes.get('/', SessionController.store);
 // routes.post('/', SessionController.store);
 // routes.put('/', SessionController.store);
 // routes.delete('/', SessionController.store);
+
+routes.use("/", restaurants);
 
 module.exports = routes;
