6b8dbd8bec83373948d78193bd5790a937282321
Haru <frankcito639@gmail.com>
2023-01-03 14:37:26 -0500

    Ruta Products

--- api/src/models/Products.js
+++ api/src/models/Products.js
@@ -1,3 +1,30 @@
+// const { DataTypes } = require("sequelize");
+// // Exportamos una funcion que define el modelo
+// // Luego le injectamos la conexion a sequelize.
+// module.exports = (sequelize) => {
+//   // defino el modelo
+//   sequelize.define("Products", {
+//     id: {
+//       type: DataTypes.UUID,
+//       defaultValue: DataTypes.UUIDV4,
+//       allowNull: false,
+//       primaryKey: true,
+//     },
+//     photo: {
+//       type: DataTypes.TEXT,
+//     },
+//     name: {
+//       type: DataTypes.STRING,
+//     },
+//     price: {
+//       type: DataTypes.NUMBER,
+//     },
+//     descriptions: {
+//       type: DataTypes.STRING,
+//     },
+//   });
+// };
+
 const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
@@ -10,21 +37,19 @@ module.exports = (sequelize) => {
       allowNull: false,
       primaryKey: true,
     },
-    photo: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    price: {
-      type: DataTypes.NUMBER,
-      allowNull: false,
+    photo: {
+      type: DataTypes.TEXT,
+      defaultValue: null,
     },
-    descriptions: {
+    price: {
+      type: DataTypes.INTEGER,
+    },
+    description: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
   });
 };
