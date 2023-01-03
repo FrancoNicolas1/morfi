c00d104fb3ebdeb781e6ae7d8db99352666035fa
Haru <frankcito639@gmail.com>
2023-01-03 14:38:33 -0500

    Ruta Products

--- api/src/models/Products.js
+++ api/src/models/Products.js
@@ -1,30 +1,3 @@
-// const { DataTypes } = require("sequelize");
-// // Exportamos una funcion que define el modelo
-// // Luego le injectamos la conexion a sequelize.
-// module.exports = (sequelize) => {
-//   // defino el modelo
-//   sequelize.define("Products", {
-//     id: {
-//       type: DataTypes.UUID,
-//       defaultValue: DataTypes.UUIDV4,
-//       allowNull: false,
-//       primaryKey: true,
-//     },
-//     photo: {
-//       type: DataTypes.TEXT,
-//     },
-//     name: {
-//       type: DataTypes.STRING,
-//     },
-//     price: {
-//       type: DataTypes.NUMBER,
-//     },
-//     descriptions: {
-//       type: DataTypes.STRING,
-//     },
-//   });
-// };
-
 const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
