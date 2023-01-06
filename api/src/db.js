e878a2dfd28e38043da34a21da167609070afe4d
Haru <frankcito639@gmail.com>
2023-01-06 13:02:36 -0500

    Volvio Products

--- api/src/db.js
+++ api/src/db.js
@@ -14,7 +14,7 @@ const {
 } = process.env;
 
 const sequelize = new Sequelize(
-  `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/morfi`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
